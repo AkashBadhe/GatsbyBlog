9590dff78afd719460caa40b49850db74bd00f3f
Akash Badhe <Akash.Badhe@silicus.com>
2019-06-20 18:09:04 +0530

    code cleanup

---
+++ site/src/pages/index.js
@@ -0,0 +1,58 @@
+import React from "react"
+import { graphql } from "gatsby"
+import "../styles/global.css"
+import Layout from "../layouts/one-col-layout";
+import BootStrapCarousel from '../components/bootstrap-carousel'
+import { Badge, Container } from 'react-bootstrap'
+import BlogCards from '../components/cards/blog-cards'
+
+const IndexPage = ({ data }) => {
+  return (
+    <Layout>
+      <div >
+        <BootStrapCarousel data={data.allStrapiCarousel.edges} />
+        <br></br>
+        <Container>
+          <BlogCards cards={data.allStrapiArticle.edges} />
+        </Container>
+      </div>
+    </Layout>
+  )
+}
+
+export default IndexPage
+export const query = graphql`  
+query {allStrapiCarousel {
+      edges {
+        node {
+          Title
+          subtitle
+          Image {
+            childImageSharp {
+              resize(width: 1200, height: 400){
+                src
+              }
+            }
+          }
+          description
+        }
+      }
+    }
+  	allStrapiArticle{
+      edges{
+        node{
+          id
+          title
+          content
+          image {
+            childImageSharp {
+              resize(width: 235, height: 160){
+                src
+              }
+            }
+          }
+        }
+      }
+    }
+  }`
+
